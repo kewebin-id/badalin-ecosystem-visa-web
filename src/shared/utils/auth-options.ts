60e25677e10ec053a54e5cd6f8721051c18edaeb
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2026-04-11 14:22:05 +0700

    fix: login

--- src/shared/utils/auth-options.ts
+++ src/shared/utils/auth-options.ts
@@ -112,7 +112,7 @@ export const authOptions: NextAuthOptions = {
   },
 
   pages: {
-    signIn: '/login',
+    signIn: '/auth/login',
     signOut: '/',
   },
 
@@ -123,12 +123,17 @@ export const authOptions: NextAuthOptions = {
   },
   cookies: {
     sessionToken: {
-      name: `next-auth.session-token`,
+      name:
+        process.env.NODE_ENV === 'production'
+          ? `__Secure-next-auth.session-token`
+          : `next-auth.session-token`,
       options: {
         httpOnly: true,
         sameSite: 'lax',
         path: '/',
-        secure: process.env.NODE_ENV === 'production',
+        secure:
+          process.env.NODE_ENV === 'production' &&
+          !process.env.NEXTAUTH_URL?.includes('localhost'),
       },
     },
   },
