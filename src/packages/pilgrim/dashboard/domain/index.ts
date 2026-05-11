aaacf35bc47b96a94b168e96ff7c2b7efe18c568
Brilian Rachmad Nurwachidin <76217952+brilianrn@users.noreply.github.com>
2026-05-11 16:31:05 +0700

    feat: refund

--- src/packages/pilgrim/dashboard/domain/index.ts
+++ src/packages/pilgrim/dashboard/domain/index.ts
@@ -3,4 +3,5 @@ import { ITransaction } from '../../transaction/domain/transaction';
 export interface IVisaHistory extends ITransaction {
   transactionId: string;
   destinationDate: string;
+  flightRoute: string;
 }
